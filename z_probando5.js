4212b9aaa3ef5d660b6644ea4658e6d0e3d9b8e5
hernandiaz94 <hernan.diiaz@gmail.com>
2023-03-07 20:04:23 -0300

    archivos de clase de pre curso henry hasta m7

---
+++ z_probando5.js
@@ -0,0 +1,23 @@
+/* let cadena = ["hola","chau"];
+console.log(cadena.reverse()); */
+
+class Persona {
+    constructor(nombre, apellido, edad, domicilio) {
+     this.nombre = nombre;
+     this.apellido = apellido;
+     this.edad = edad;
+     this.domicilio = domicilio;
+   }
+ 
+    detalle() {
+       return {
+         nombre: this.nombre,
+         apellido: this.apellido,
+         edad: this.edad,
+         domicilio: this.domicilio,
+       };
+    }
+ }
+
+ const persona1 = new Persona("Juan", "Pérez", 30, "Calle Falsa 123");
+console.log(persona1.detalle());
